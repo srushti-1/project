e15e0204ef65c56fe778d88b6e88a77ee757a56c
srushti-1 <73881203+srushti-1@users.noreply.github.com>
2020-12-22 09:05:28 +0530

    Add files via upload

---
+++ Mango.js
@@ -0,0 +1,15 @@
+class Mango{
+    constructor(x,y){
+        var options ={
+            isStatic : false,
+            restitution: 0,
+            density:1,
+            friction : 1
+        }
+
+        
+      }
+      display(){
+
+      }
+      }
---
+++ Stone.js
@@ -0,0 +1,16 @@
+class Stone{
+    constructor(x,y){
+        var options ={
+            isStatic : true,
+            restitution: 0,
+            density:1
+        }
+
+        
+      }
+      display(){
+
+      }
+      }
+    
+
